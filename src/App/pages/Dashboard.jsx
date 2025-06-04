4015373dcd5bdb612caaf839842bc80343a419d5
Dandy <phoenixmations@gmail.com>
2025-06-04 21:50:10 +0700

    fixed probably

--- src/App/pages/Dashboard.jsx
+++ src/App/pages/Dashboard.jsx
@@ -21,7 +21,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import placeholder from "@/assets/placeholder.jpg";
+import placeholder from "../../assets/placeholder.jpg";
 
 //warna
 const statusOptions = ["Belum Jalan", "Dalam Progress", "Fixed"];
