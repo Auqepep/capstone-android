ebc00957c5739eaae26dc32c3f5088ea80eaf00f
Dandy <phoenixmations@gmail.com>
2025-06-06 22:59:17 +0700

    env changes

--- src/App/pages/Login.jsx
+++ src/App/pages/Login.jsx
@@ -45,7 +45,7 @@ export default function Login() {
     console.log("Login data to submit:", { email, password });
 
     try {
-      const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
       const response = await fetch(`${apiUrl}/user/login`, {
         method: "POST",
         headers: {
